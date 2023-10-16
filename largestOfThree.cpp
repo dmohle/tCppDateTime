0d72e7420bf47a17dbe75d13cb04af731ae57627
dmohle <10038913+dmohle@users.noreply.github.com>
2023-10-15 19:56:59 -0700

    Add files via upload

---
+++ largestOfThree.cpp
@@ -0,0 +1,43 @@
+// largestOfThree.cpp
+//
+// Programmer's name: Dennis Mohle
+// Date: Aug 16, 2023
+//
+// Description of program:
+//
+// TODO: Describe your program in terms of input, processing, and output
+//
+// This program will input...
+//
+//
+// References:
+//
+// Getting started:
+// https://www.w3schools.com/cpp/cpp_getstarted.asp
+// User Input:
+// https://www.w3schools.com/cpp/cpp_user_input.asp
+// Strings:
+// https://www.w3schools.com/cpp/cpp_strings.asp
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "\n\n Welcome to the largest of Three Program\n\n";
+
+  // Get the user's name
+  string userName = "";
+  cout << "\n Please enter your name: ";
+  cin >> userName;
+
+  //
+
+
+
+
+
+  return 0;
+
+}
+
+
